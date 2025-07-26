98a0d89f06226f8d112e6669db4374ec6a3d489f
krispykeenz <37831909@student.g.nwu.ac.za>
2025-07-26 15:55:59 +0200

    backend url update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://your-backend-deployment.railway.app/api' // Update with your actual backend URL
+  apiUrl: 'virtual-cv-production.up.railway.app'
 };
